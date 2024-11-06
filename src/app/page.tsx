f05baf7b75ae7835a74c44e9cfecee7b8a145e49
Ankur Kaushal <97340597+Avenster@users.noreply.github.com>
2024-11-07 01:54:35 +0530

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
 // import Image from "next/image";
 import '../css/app.css'
 // import { Button } from "@/components/ui/button"
-// import HeroSection from "../components/HeroSection";
-// import Features from "../components/Features";
-// import Navbar from "../components/navbar";
-// import Upload from "../components/Upload";
-// import Footer from "../components/footer";
-import RegisterPage from "../components/RegisterPage";
+import HeroSection from "../components/HeroSection";
+import Features from "../components/Features";
+import Navbar from "../components/navbar";
+import Upload from "../components/Upload";
+import Footer from "../components/footer";
+// import RegisterPage from "../components/RegisterPage";
 
 export default function Home() {
   return (
     <div className="App">
-      {/* <Navbar/>
+      <Navbar/>
       <HeroSection/>
       <Features/>
      
       <Upload />
       
-      <Footer/> */}
+      <Footer/>
 
-      <RegisterPage />
+      {/* <RegisterPage /> */}
       
 
 
